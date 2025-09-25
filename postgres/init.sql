815f31c2383c07e54c56f9a0888cf6e203250952
Sats-Script <satish.matapathi@gmail.com>
2025-09-25 19:00:07 +0530

    .Net project deployed in cloud using all DevOps tools

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,4 +1,10 @@
--- Initial seed (optional). EF Core migration will create schema.
+CREATE TABLE "Products" (
+  "Id" serial PRIMARY KEY,
+  "Name" text NOT NULL,
+  "Price" numeric(10,2) NOT NULL,
+  "InStock" boolean NOT NULL
+);
+
 INSERT INTO "Products" ("Name","Price","InStock") VALUES
-('Notebook', 199.00, true),
-('Pen', 25.50, true);
+  ('Notebook', 199.00, true),
+  ('Pen', 25.50, true);
